fe674183da5ca1c53637b7ca835939e5961b6bc6
TysonRayJones <tyson.jones@monash.edu>
2017-06-30 23:24:44 -0400

    added TimeSteps option to GetEigenmodes

--- project/EigenFunctions/Main.m
+++ project/EigenFunctions/Main.m
@@ -7,6 +7,7 @@ Package["EigenFunctions`"]
 
 PackageExport[NumberOfModes]
 PackageExport[NumberOfPoints]
+PackageExport[TimeSteps]
 
 
 (* function exports *)
@@ -17,6 +18,7 @@ PackageExport[GetPotentialMatrix]
 PackageExport[GetHamiltonianMatrix]
 PackageExport[GetEigenmodes]
 PackageExport[Discretise]
+
 
 
 (* public functions *)
@@ -43,6 +45,17 @@ Discretise::usage =
 (* function definitions *)
 
 
+(* times and prints an expression's evaluation *)
+TimeEigFlag = False;
+SetAttributes[EchoTiming, HoldFirst]
+EchoTiming[expr_, label_:"Timing"] := 
+	If[
+		TimeEigFlag,
+		Last @ Echo[AbsoluteTiming @ expr, label <> " ", First],
+		expr
+	]
+
+
 
 NormaliseDiscrete[psi_, grid_] :=
 	With[
@@ -63,11 +76,12 @@ GetLaplacianMatrix[grid_] :=
 			{
 				(* 4th order accuracy of 1D finite difference 2nd-deriv *)
 				{i_,i_} :> -5/2,
-				{i_,j_} :> 4/3   /; Abs[i-j] == 1,
-				{i_,j_} :> -1/12 /; Abs[i-j] == 2
+				{i_,j_} /; Abs[i-j] == 1 :> 4/3,
+				{i_,j_} /; Abs[i-j] == 2 :> -1/12 
 			},
 			{numPoints, numPoints}
-		] / gridSpace^2
+		]
+		/ gridSpace^2
 	]
 		 
 
@@ -86,44 +100,63 @@ GetPotentialMatrix[potential_] :=
 GetHamiltonianMatrix[potential_, grid_] :=
 
 	(* single-particle, non-dimensional, inertial, kinetic energy + potential *)
-	-1/2 GetLaplacianMatrix[grid] + GetPotentialMatrix[potential]
+	-(1/2) GetLaplacianMatrix[grid] +  GetPotentialMatrix[potential]
 
 
 
 Options[GetEigenmodes] = {
 
 	"NumberOfModes" -> 10,
-	"NumberOfPoints" -> 100   (* overrides Discretise default *)
+	"NumberOfPoints" -> 100,   (* overrides Discretise default *)
+	
+	"TimeSteps" -> False
 
 }
 
 GetEigenmodes[potential_, domain_, OptionsPattern[]] :=
-	
+
 	(* will assume NumberOfPoints < numerical points in grid *)
 	Module[
 		{x, H, V, \[Lambda], \[Phi]},
 		
+		(* re-instate timing and printing (if true) *)
+		TimeEigFlag = OptionValue["TimeSteps"];
+		
 		(* get discretised grid and potential lists *)
-		{x, V} = Discretise[
-			potential, 
-			domain, 
-			NumberOfPoints -> OptionValue["NumberOfPoints"]
-		];
+		{x, V} = EchoTiming[
+			Discretise[
+				potential, 
+				domain, 
+				NumberOfPoints -> OptionValue["NumberOfPoints"]
+			],
+			"discretising potential"];
 		
 		(* fabricate high potential at end-points to enhance stability *)
 		V[[1]] = V[[-1]] = 10^5;
 		
 		(* build the Hamiltonian matrix *)
-		H = GetHamiltonianMatrix[V, x];
+		H = EchoTiming[
+			GetHamiltonianMatrix[V, x], 
+			"building H matrix"];
 		
 		(* diagonalise the matrix *)
-		{\[Lambda], \[Phi]} = Eigensystem[N[H], -OptionValue["NumberOfModes"]];
+		{\[Lambda], \[Phi]} = EchoTiming[
+			Eigensystem[N[H], -OptionValue["NumberOfModes"]],
+			"diagonalising H matrix"];
 
 		(* reorder eigenfunctions by increasing energy eigenvalue *)
 		{\[Lambda], \[Phi]} = {\[Lambda][[#]], \[Phi][[#]]}& @ Ordering[\[Lambda]];
 		
 		(* normalise eigenfunction lists *)
-		{\[Lambda], \[Phi]} = {\[Lambda], NormaliseDiscrete[#, x]& /@ \[Phi]}
+		{\[Lambda], \[Phi]} = EchoTiming[
+			{\[Lambda], NormaliseDiscrete[#, x]& /@ \[Phi]},
+			"normalising eigenfunctions"];
+		
+		(* disable timing and printing *)
+		TimeEigFlag = False;
+		
+		(* return *)
+		{\[Lambda], \[Phi]}
 	]
 	
 	
